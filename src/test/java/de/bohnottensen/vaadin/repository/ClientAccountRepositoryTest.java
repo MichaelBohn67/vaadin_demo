da7592fad2e4cd4cfafe3b668b33278b503f191e
Michael  Bohn <michaelbohn67@gmail.com>
2021-08-06 17:49:04 +0200

    added more Tests

--- src/test/java/de/bohnottensen/vaadin/repository/ClientAccountRepositoryTest.java
+++ src/test/java/de/bohnottensen/vaadin/repository/ClientAccountRepositoryTest.java
@@ -1,5 +1,7 @@
 package de.bohnottensen.vaadin.repository;
 
+import de.bohnottensen.vaadin.model.Account;
+import de.bohnottensen.vaadin.model.Client;
 import de.bohnottensen.vaadin.model.ClientAccount;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +15,26 @@ class ClientAccountRepositoryTest {
 
   @Autowired
   ClientAccountRepository repository;
+  @Autowired
+  AccountRepository accountRepository;
+  @Autowired
+  ClientRepository clientRepository;
 
   @Autowired
   TestEntityManager entityManager;
 
   @Test
   void givenClientAccount_whenSaved_thenSuccess() {
+    Account account = new Account();
+    account.setId(1L);
+    accountRepository.save(account);
+    Client client = new Client();
+    client.setId(1L);
+    clientRepository.save(client);
     ClientAccount newClientAccount = new ClientAccount();
-
+    newClientAccount.setId(1L);
+    newClientAccount.setIdAccount(account);
+    newClientAccount.setIdClient(client);
     ClientAccount insertedClientAccount = repository.save(newClientAccount);
 
     assertThat(entityManager.find(ClientAccount.class, insertedClientAccount.getId())).isEqualTo(newClientAccount);
